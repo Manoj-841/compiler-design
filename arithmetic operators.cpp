a40f8b0a3ff7caae31fcf288bbd34b5d74d912ca
Manoj-841 <113409747+Manoj-841@users.noreply.github.com>
2023-01-26 10:05:23 +0530

    Add files via upload

---
+++ arithmetic operators.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a=60,b=20,addition,subtraction,multiplication,division,modulus;
+	addition=a+b;
+	subtraction=a-b;
+	multiplication=a*b;
+	division=a/b;
+	modulus=a%b;
+	printf("addition of a,b:%d\n",addition);
+	printf("subtractionof a,b:%d\n",subtraction);
+	printf("multiplication of a,b:%d\n",multiplication);
+	printf("division of a,b:%d\n",division);
+	printf("modulus of a,b:%d\n",modulus);
+	return 0;
+}
